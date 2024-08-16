b31b6038bc525fd7e2cc3c644122c1cc49d7d5a8
Adam Karpinski <kogut0000@gmail.com>
2024-08-16 14:59:54 +0200

    fix: import from, not whole module

--- measure.py
+++ measure.py
@@ -1,9 +1,11 @@
 # for measuring parameters of uC
 # time, memmory
     
-import gc
-import os
-import time
+from os import statvfs, dupterm
+from gc import collect, mem_alloc, mem_free
+from time import ticks_us, ticks_diff
+from micropython import mem_info, qstr_info, stack_use, opt_level
+from dup import DUP
 
 
 def help():
@@ -18,23 +20,24 @@ def help():
 def timed_function(f, *args, **kwargs):
     myname = str(f).split(' ')[1]
     def new_func(*args, **kwargs):
-        t = time.ticks_us()
+        t0 = ticks_us()
         result = f(*args, **kwargs)
-        delta = time.ticks_diff(time.ticks_us(), t)
+        t1 = ticks_us()
+        delta = ticks_diff(t1, t0)
         print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
         return result
     return new_func
 
 
 def avilable_space():
-    stat = os.statvfs('//')
+    stat = statvfs('//')
     return ('{0} MB'.format((stat[0]*stat[3])/1048576))
 
 
-def avilable_ram():
-    gc.collect() # collect garbage
-    free = gc.mem_free()
-    allocated = gc.mem_alloc()
+def avilable_ram(short = True):
+    collect() # collect garbage
+    free = mem_free()
+    allocated = mem_alloc()
     total = free + allocated
     percentage = '{0:.2f}%'.format(free/total*100)
 
@@ -44,31 +47,34 @@ def avilable_ram():
         return ('Total:{0} Free:{1} ({2})'.format(total, free, percentage))
 
 
+
+# ---------------------------- Is it needed? V ----------------------------
+
 # time_mem_result = [None]*3 # Time, RAM, File_space
 # global time_mem_result
 time_mem_result = {"func":"", "time":"", "ram":"", "space":""}
 
-# any function timig by "@timed_function" decorator
+# any function timig and memory by "@time_mem" decorator
 def time_mem(f, *args, **kwargs):
     myname = str(f).split(' ')[1]
     def new_func(*args, **kwargs):
         # global time_mem_result
 
-        gc.collect() # collect garbage
+        collect() # collect garbage
 
         # check uC parameters before function
-        ram = gc.mem_alloc()
-        stat = os.statvfs('//')
+        ram = mem_alloc()
+        stat = statvfs('//')
         file_space = (stat[0]*stat[3])#/1048576   # avilable space in MB
-        t = time.ticks_us()
+        t = ticks_us()
 
         # run function
         result = f(*args, **kwargs)
 
         # check uC parameters after function and calculate difference
-        t_delta = time.ticks_diff(time.ticks_us(), t)
-        ram_delta = gc.mem_alloc() - ram
-        stat = os.statvfs('//')
+        t_delta = ticks_diff(ticks_us(), t)
+        ram_delta = mem_alloc() - ram
+        stat = statvfs('//')
         file_space_delta = (stat[0]*stat[3]) - file_space #/1048576   # avilable space in MB
 
         # time_mem_result[0] = t_delta
@@ -91,27 +97,27 @@ def time_mem(f, *args, **kwargs):
         return result
     return new_func
 
-# any function timig by "@timed_function" decorator
+# any function timig and memory by "@t_m" decorator
 def t_m(n, f, *args, **kwargs):
     myname = str(f).split(' ')[1]
     # def new_func(*args, **kwargs):
 
-    gc.collect() # collect garbage
+    collect() # collect garbage
 
     # check uC parameters before function
-    ram = gc.mem_alloc()
+    ram = mem_alloc()
     # stat = os.statvfs('//')
     # file_space = (stat[0]*stat[3])#/1048576   # avilable space in MB
-    t0 = time.ticks_us()
+    t0 = ticks_us()
 
     # run function
     for _ in range(n):
         f(*args, **kwargs)
 
     # check uC parameters after function and calculate difference
-    t1 = time.ticks_us()
-    t_delta = time.ticks_diff(t1, t0)
-    ram_delta = gc.mem_alloc() - ram
+    t1 = ticks_us()
+    t_delta = ticks_diff(t1, t0)
+    ram_delta = mem_alloc() - ram
     # stat = os.statvfs('//')
     # file_space_delta = (stat[0]*stat[3]) - file_space #/1048576   # avilable space in MB
 
@@ -135,6 +141,8 @@ def print_result(result):
     print('Time =\t{:.3f} ms'.format(result["time"]/1000))
     print('RAM =\t{} b'.format(result["ram"]))
     print('Space =\t{} MB'.format(result["space"]/1048576))
+
+# ---------------------------- Is it needed? ^ ----------------------------
 
 
 # exec(open("measure.py").read())
