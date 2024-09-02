5755d464218fbf3f4919b9b928090e1c53c6e6d3
Adam Karpinski <kogut0000@gmail.com>
2024-09-02 14:51:04 +0200

    feat: load file with dump from REPL

--- bigPython/code/output_analyze.py
+++ bigPython/code/output_analyze.py
@@ -7,3 +7,8 @@ fr_qstr = "qstr_info"
 fr_mem = "mem_info"
 fr_before = "before"
 fr_after = "after"
+
+# chose file with console dump to open
+path_in = "bigPython/results/test_results.txt"
+# path_in = "bigPython/results/g_var_6_results.txt"
+file = open(path_in, 'r').read()
