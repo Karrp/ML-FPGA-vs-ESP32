19508af63b37d877380207b36a367178842202b1
Adam Karpinski <kogut0000@gmail.com>
2024-09-02 14:54:55 +0200

    feat: read measurements from loaded file

--- bigPython/code/output_analyze.py
+++ bigPython/code/output_analyze.py
@@ -1,3 +1,5 @@
+from analyze import qstr_val, mem_val
+
 # strings for frame (fr)
 # d^-^b ["---> start"/"<--- stop"], <name>, <function>, ["before"/"after"]
 fr_sign = "d^-^b" # start of the frame
@@ -12,3 +14,34 @@ fr_after = "after"
 path_in = "bigPython/results/test_results.txt"
 # path_in = "bigPython/results/g_var_6_results.txt"
 file = open(path_in, 'r').read()
+file = file.split(fr_sign + " ")
+
+results = []
+for element in file:
+    if element.find(fr_start) != -1:
+        head = (element.splitlines()[0]).split(", ")[1:]
+        name, fun, when = head # maybe to dictionary? or class?
+
+        data = element[element.find("\n")+1:]
+
+        if fun == fr_qstr:
+            data = qstr_val(data)
+        elif fun == fr_mem:
+            data = mem_val(data)
+            # pass
+
+        results.append([head, data])
+
+        # analyze rest of data or pass just in string
+    elif element.find(fr_stop) != -1:
+        # check if frame stop is ending last data frame (head parameters are aqual)
+        start_head = results[-1][0]
+        head = (element.splitlines()[0]).split(", ")[1:]
+        if head[0] == start_head[0] and head[1] == start_head[1]:
+            pass
+            # print(head[0], head[1], "fine")
+        else:
+            print(head[0], head[1], "problem!!!")
+        # pass
+        
+del file
