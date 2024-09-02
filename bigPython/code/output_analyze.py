2b0d78e5b56e385ea8af097df467d1c58da03766
Adam Karpinski <kogut0000@gmail.com>
2024-09-02 14:50:01 +0200

    feat: frame structure for console output analyzis

---
+++ bigPython/code/output_analyze.py
@@ -0,0 +1,9 @@
+# strings for frame (fr)
+# d^-^b ["---> start"/"<--- stop"], <name>, <function>, ["before"/"after"]
+fr_sign = "d^-^b" # start of the frame
+fr_start = "---> start"
+fr_stop = "<--- stop"
+fr_qstr = "qstr_info"
+fr_mem = "mem_info"
+fr_before = "before"
+fr_after = "after"
